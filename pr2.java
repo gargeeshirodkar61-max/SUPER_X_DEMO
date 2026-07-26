c4e4039a155aade4116d820fc25f746acb05d260
kalyaniupare06-collab <kalyaniupare06@gmail.com>
2026-07-26 22:35:07 +0530

    here my new file

---
+++ pr2.java
@@ -0,0 +1,12 @@
+package Folder2;
+
+class Practice{
+    static int x=10;
+    public static void main(String[] args) {
+        System.out.println("In main method");
+        System.out.println(x + " " + Practice.x);
+        System.out.println("End Code");
+        System.out.println("In main method");
+    }
+
+}
