ef9043b3a7e2f972bc973c0ad8df2769dd4752c5
gargeeshirodkar61-max <gargeeshirodkar61@gmail.com>
2026-07-26 20:17:17 +0530

    derived inheritance concept

--- Practice1.java
+++ Practice1.java
@@ -13,11 +13,26 @@ class Demo{
     static void run(){
         System.out.println("In run method");
     }
+}
+
+class Child extends Demo{
+    int x=100;
+    Child(){
+        System.out.println("Constructor of Child class");
+    }
+
+    void gun(){
+        System.out.println("In gun method" + x);
+    }
 
     public static void main(String[] args) {
         Demo d = new Demo();
         System.out.println(d.x);
         d.fun();
         Demo.run();
+
+        Child c = new Child();
+        System.out.println(c.x);
+        c.gun();
     }
 }
